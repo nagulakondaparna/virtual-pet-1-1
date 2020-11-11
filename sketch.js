5fd88a8625a3ee3b0f5c78b56fbb20f20c7ebedc
nagulakondaparna <71821602+nagulakondaparna@users.noreply.github.com>
2020-11-11 18:25:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,125 @@
+var dog, happyDog, database, foodS, foodStock
+var dogImg, dogHappyImg;
+var milk, milkImg;
+
+
+function preload()
+{
+  dogImg = loadImage("Dog.png");
+  dogHappyImg = loadImage("happydog.png");
+  milkImg = loadImage("milk.png");
+  
+
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(500, 500);
+  
+  dog = createSprite(250,250,10,10);
+  dog.addImage(dogImg);
+  dog.scale = 0.15;
+
+  emo = createSprite(200,200,1,1);
+  
+  foodStock = database.ref('food');
+  foodStock.on("value",readStock);
+  foodStock.set(50);
+  
+  milk = createSprite(140,435,10,10);
+  milk.addImage(milkImg);
+  milk.scale = 0.025;
+
+  milk1 = createSprite(210,280,10,10);
+  milk1.addImage(milkImg);
+  milk1.scale = 0.025;
+  milk1.visible = false;
+
+
+  for (var i = 5; i < 500; i=i+10) 
+{
+
+var dot = createSprite(i, 5, 3, 3);
+dot.shapeColor = "blue";
+
+}
+for (var i = 5; i < 500; i=i+10) 
+{
+
+var dot1 = createSprite(i, 495, 3, 3);
+dot1.shapeColor = "blue";
+
+}
+for (var i = 5; i < 500; i=i+10) 
+{
+
+var dot1 = createSprite(495,i, 3, 3);
+dot1.shapeColor = "blue";
+
+}
+for (var i = 5; i < 500; i=i+10) 
+{
+
+var dot1 = createSprite(5,i, 3, 3);
+dot1.shapeColor = "blue";
+
+}
+}
+
+
+function draw() {  
+  background("pink")
+
+  if(foodS !== 0){
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogHappyImg);
+    milk1.visible = true;
+
+   
+  }
+
+  if(keyWentUp(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg);
+    milk1.visible = false;
+  }
+}
+
+if(foodS == 0){
+  
+  dog.addImage(dogImg);
+  foodS = 50;
+
+}
+
+
+
+  drawSprites();
+  textSize(17);
+  fill("black");
+  text("I am your Puppy 🐶Shiro..😍 I am Hungry ",100,150);
+  fill("black");
+  text("Long Press up arrow key to feed your pet Dog Shiro",50,50);
+  fill("black");
+  text("Milk Bottles Remaining  "+foodS,170,440);
+}
+
+function readStock(data)
+{
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x = 0;
+  }else{
+    x=x-1
+  }
+
+  database.ref('/').update({
+    food:x
+  })
+}
+
